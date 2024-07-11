dea6010fde1c10cd66517c7efbeb8e51d2e8426a
Robson Cruz <rcflorestal@yahoo.com.br>
2024-07-11 17:26:09 -0300

    Analog In

---
+++ 8_Analog_In/analog_in1.ino
@@ -0,0 +1,16 @@
+/* C++ code
+* This code reads the analog input on pin A5 and sends the read value to the serial
+* monitor every 500 milliseconds.
+*/
+void setup()
+{
+  pinMode(A5, INPUT);
+  // Initialize Serial Communication at 9600
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  Serial.println(analogRead(A5));
+  delay(500);
+}
